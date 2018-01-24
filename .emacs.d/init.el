e5d0df035bbe1614e1cbe43ac948edf80e124390
milk1000cc <info@milk1000.cc>
2018-01-24 16:23:28 +0900

    ~/.cask が emacs のロードパスに追加されるように

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,4 @@
-;; load-pathを追加する関数を定義
-(defun add-to-load-path (&rest paths)
+(defun add-to-load-path-recursively (&rest paths)
   (let (path)
     (dolist (path paths paths)
       (let ((default-directory
@@ -8,12 +7,12 @@
         (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
             (normal-top-level-add-subdirs-to-load-path))))))
 
-;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
-(add-to-load-path "public_repos")
+(add-to-load-path-recursively "public_repos")
 
 (require 'package)
 (package-initialize)
 
+(add-to-list 'load-path "~/.cask")
 (require 'cask)
 (cask-initialize)
 
