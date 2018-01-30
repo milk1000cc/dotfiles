75c097f9dec9aa6ab23de50c673768e626e313f2
milk1000cc <info@milk1000.cc>
2018-01-30 18:53:44 +0900

    Emacs の rails-minor-mode で、C-c C-c l でログを開くように

--- .emacs.d/inits/01_emacs-rails.el
+++ .emacs.d/inits/01_emacs-rails.el
@@ -3,3 +3,6 @@
 
 ;; C-c C-p で、コントローラとビューのファイル切り替え
 (define-key rails-minor-mode-map (kbd "C-c C-p") 'rails-lib:run-primary-switch)
+
+;; C-c C-c l で、ログを開く
+(define-key rails-minor-mode-map (kbd "C-c C-c l") 'rails-log:open)
