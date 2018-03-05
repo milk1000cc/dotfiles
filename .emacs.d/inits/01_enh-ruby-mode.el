d4220f1c88a0f4ef1f0ed6b657ef8b2a1cfd6710
milk1000cc <info@milk1000.cc>
2018-03-05 11:15:11 +0900

    Emacs: shebang から enh-ruby-mode が選択されるように

--- .emacs.d/inits/01_enh-ruby-mode.el
+++ .emacs.d/inits/01_enh-ruby-mode.el
@@ -13,8 +13,9 @@
    '(("^[^\n]\\{80\\}\\(.*\\)$" 1 font-lock-warning-face t))))
 (add-hook 'enh-ruby-mode-hook 'highlight-too-long-line)
 
+;; Ruby のファイルが enh-ruby-mode で開くように
 (add-to-list 'auto-mode-alist
              '("\\(?:\\.rb\\|ru\\|rake\\|thor\\|jbuilder\\|gemspec\\|podspec\\|/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\)\\'" . enh-ruby-mode))
-
 (add-to-list 'auto-mode-alist '("\\.god\\'" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.cap\\'" . enh-ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . enh-ruby-mode))
