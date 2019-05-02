5c38c9dbc8978d6a3ea11eafca9b7593d936d2cb
milk1000cc <info@milk1000.cc>
2019-05-02 11:14:57 +0900

    Emacs: js2-mode で Routes キーワードがハイライトされないように
    js-routes (https://github.com/railsware/js-routes) で使用

--- .emacs.d/inits/01_js2-mode.el
+++ .emacs.d/inits/01_js2-mode.el
@@ -3,6 +3,6 @@
 (setq js2-basic-offset 2)
 (setq js2-include-node-externs t)
 (setq js2-strict-missing-semi-warning nil)
-(setq js2-global-externs '("location" "gon"))
+(setq js2-global-externs '("location" "gon" "Routes"))
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
