c52f7cececfb7080f5ff7d986da063f705a455b4
milk1000cc <info@milk1000.cc>
2020-01-04 20:21:04 +0900

    macos の zsh で、環境変数 RUBY_CONFIGURE_OPTS を設定するように
    brew info ruby-build 参照

--- .zshrc.local/macos.zsh
+++ .zshrc.local/macos.zsh
@@ -1,10 +1,14 @@
+BREW_PREFIX=/usr/local/opt
+
 path=(
-    /usr/local/opt/coreutils/libexec/gnubin
+    $BREW_PREFIX/coreutils/libexec/gnubin
     $path
 )
 
 HISTFILE=~/Dropbox/zsh_history
 
 alias diff='colordiff -ur'
+
+export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$BREW_PREFIX/openssl"
 
 [[ -z $TMUX ]] && tmux
