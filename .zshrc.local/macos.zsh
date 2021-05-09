7f6d9f767321c3dfa9c57d722ee46c58e9b6abc6
milk1000cc <info@milk1000.cc>
2021-05-09 12:37:15 +0900

    zshrc で、google-cloud-sdk の設定をするように

--- .zshrc.local/macos.zsh
+++ .zshrc.local/macos.zsh
@@ -12,4 +12,8 @@ alias diff='colordiff -ur'
 
 export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$BREW_PREFIX/openssl"
 
+# google-cloud-sdk
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+
 [[ -z $TMUX ]] && tmux
