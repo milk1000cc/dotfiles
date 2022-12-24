2ac458afd97fefde0078f3e164ce48e64c090f06
milk1000cc <info@milk1000.cc>
2022-12-24 14:53:47 +0900

    rails template: GitHub Actions での CI workflow ファイルを追加

--- rails/template/template.rb
+++ rails/template/template.rb
@@ -39,6 +39,8 @@ CODE
 copy_file "#{ __dir__ }/config/initializers/generators.rb", 'config/initializers/generators.rb'
 copy_file "#{ __dir__ }/config/initializers/locale.rb", 'config/initializers/locale.rb'
 
+copy_file "#{ __dir__ }/.github/workflows/ci.yml", '.github/workflows/ci.yml'
+
 after_bundle do
   run 'bundle binstubs rspec-core'
 
