3fee82a58768e54dedbd61dcd0cbbc72b0cdceee
milk1000cc <info@milk1000.cc>
2023-01-09 11:42:00 +0900

    rails template: font awesome の読み込みを個別ファイルに分割

--- rails/template/app/assets/config/manifest.js
+++ rails/template/app/assets/config/manifest.js
@@ -5,4 +5,7 @@
 //= link sanitize.css/forms.css
 //= link sanitize.css/assets.css
 //= link sanitize.css/reduce-motion.css
-//= link @fortawesome/fontawesome-free/css/all.css
+//= link @fortawesome/fontawesome-free/css/fontawesome.css
+//= link @fortawesome/fontawesome-free/css/brands.css
+//= link @fortawesome/fontawesome-free/css/solid.css
+//= link @fortawesome/fontawesome-free/css/regular.css
