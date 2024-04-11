03b8474228d081af94e23fc8af9f12780e954565
milk1000cc <info@milk1000.cc>
2024-04-11 11:17:51 +0900

    rails template: Font Awesome のファイルを all.min.css に統一

--- rails/template/app/assets/config/manifest.js
+++ rails/template/app/assets/config/manifest.js
@@ -5,7 +5,4 @@
 //= link sanitize.css/forms.css
 //= link sanitize.css/assets.css
 //= link sanitize.css/reduce-motion.css
-//= link @fortawesome/fontawesome-free/css/fontawesome.css
-//= link @fortawesome/fontawesome-free/css/brands.css
-//= link @fortawesome/fontawesome-free/css/solid.css
-//= link @fortawesome/fontawesome-free/css/regular.css
+//= link @fortawesome/fontawesome-free/css/all.min.css
