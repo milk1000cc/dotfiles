5e86b2698975ef1cc50f6598b31ebecc0f37ac1c
milk1000cc <info@milk1000.cc>
2026-08-25 09:57:11 +0900

    Revert "emacs: 自動ポップアップ補完を廃止"
    This reverts commit c6a4bcb8f0fb5961263259bb88428c57249b651f.

--- emacs/init.el
+++ emacs/init.el
@@ -224,11 +224,19 @@
 
 ;; corfu
 (use-package corfu
-  :bind ("C-c o" . completion-at-point)
+  :bind ("C-c o" . my/corfu-toggle-auto)
   :init
   (setq completion-ignore-case t)
   :config
-  (global-corfu-mode 1))
+  (global-corfu-mode 1)
+  (defun my/corfu-toggle-auto ()
+    (interactive)
+    (setq-local corfu-auto (not corfu-auto))
+    (when corfu-mode
+      (corfu-mode -1)
+      (corfu-mode 1))
+    (message "Corfu auto completion: %s"
+             (if corfu-auto "ON" "OFF"))))
 
 ;; corfu-terminal
 (use-package corfu-terminal
